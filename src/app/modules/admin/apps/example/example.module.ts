b5a20095128440f8b26415190b05bc4bff03706c
khanhddk@git.vn <khanhddk@git.vn>
2025-02-12 15:41:42 +0700

    update

--- src/app/modules/admin/apps/example/example.module.ts
+++ src/app/modules/admin/apps/example/example.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Route, RouterModule } from '@angular/router';
+import { FuseScrollbarModule } from '@fuse/directives/scrollbar';
+import { MaterialModule } from 'app/material/material.module';
 import { ExampleComponent } from 'app/modules/admin/apps/example/example.component';
+import { SharedModule } from 'app/shared/shared.module';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 const exampleRoutes: Route[] = [
     {
@@ -14,7 +19,12 @@ const exampleRoutes: Route[] = [
         ExampleComponent
     ],
     imports     : [
-        RouterModule.forChild(exampleRoutes)
+        RouterModule.forChild(exampleRoutes),
+        SharedModule,
+        NgxFileDropModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        FuseScrollbarModule
     ]
 })
 export class ExampleModule
